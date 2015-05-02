5ae94845ed2f2187702c05e8f6a9c619a75fbcb5
Clayton Hawken <claytonhawken@gmail.com>
2015-05-02 17:14:02 -0500

    Testing Merge Conflict

---
+++ src/utilities/PathFinder.java
@@ -0,0 +1,22 @@
+package utilities;
+
+
+/*
+ * This class is designed to look at a current level (map + towers placed) and decided the optimal path for the creep, return a list of vertices
+ * Additionally the class can look at a current level and a proposed tower placement to return the list of vertices. Returns an empty list for an invalid path
+ * 
+ */
+
+
+
+
+
+
+public class PathFinder {
+
+	public static void main(String[] args){
+		int j = 42;
+		System.out.println("Tim is a nigga" + j);
+	}
+	
+}
