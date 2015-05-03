53c725d24bd5601b685cb4fa9cfd31cc37d287fa
Clayton Hawken <claytonhawken@gmail.com>
2015-05-03 18:34:28 -0500

    updated game logic skeleton in level

--- src/levels/Level.java
+++ src/levels/Level.java
@@ -1,9 +1,15 @@
 package levels;
 
+import java.util.ArrayList;
+
 import maps.Map;
+import maps.Wave;
+import creeps.Creep;
 import creeps.ElementType;
 import maps.Tile;
 import players.Player;
+import towers.Projectile;
+import towers.Tower;
 
 /*
  * Executes main game logic loop
@@ -12,17 +18,56 @@ public class Level {
 
 	private final Map map;
 	private final Player player;
+	private final ArrayList<Wave> creepWaves;
 
-	public Level(Player player, Map map){
+	public int round = 0; //Each round represents a specific creepwave (Or waves for multiple entrance)
+	public int tick = 0; //Specific game logic step, smallest possible difference in game states time wise
+	
+	int gold, health;
+	
+	//Currently loaded/active units
+	ArrayList<Tower> towers = new ArrayList<Tower>();
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Creep> creeps = new ArrayList<Creep>();
+	
+	
+	public Level(Player player, Map map, ArrayList<Wave> creepWaves){
 		this.player = player;
 		this.map = map;
+		this.creepWaves = creepWaves;
 	}
 	
+	//Essentially ticks
 	public void gameTick(){
-		//@TODO
+		tick++;
+		
+		//Check for new spawns from creep wave;
+		
+		for(Creep c: creeps){
+			//Update movement, direction faceing
+		}
+		
+		for(Projectile p: projectiles){
+			//Update movement, execute impact logic if needed
+		}
+
+		for(Tower t: towers){
+			//Update target, if ready fire new projectile
+		}
+		
+		//Check if round has finished from all creep being dead
 	}
 	
 	public void buildTower(ElementType type, Tile location){
 		//@TODO
 	}
+	
+	public void upgradeTower(Tower t, int index){
+		//@TODO
+	}
+	
+	public void razeTower(Tower t){
+		//@TODO
+	}
+	
 }
--- src/utilities/PathFinder.java
+++ src/utilities/PathFinder.java
@@ -1,5 +1,7 @@
 package utilities;
-
+/*
+ * Tool to grab the path for a certain level, will need to be run anytime a tower is built
+ */
 public class PathFinder {
 	
 }
