5733ade69a23ce1e8ee007205ef220540285286c
qsik <qsik777@gmail.com>
2015-05-04 22:19:37 -0500

    Added generic methods to ProjectileEffect. Encapsulated Type into Effect

--- src/towers/ProjectileEffect.java
+++ src/towers/ProjectileEffect.java
@@ -1,8 +1,27 @@
 package towers;
+
+import creeps.Creep;
+
 /*
  * An on hit effect that can be applied from a projectile
  * information such as chance to hit, duration and effect are included
  */
 public class ProjectileEffect {
+	public enum ProjectileEffectType {
+		SLOW, SNARE, STUN, DAMAGE_OVER_TIME, MORTAL_STRIKE, ;
+	}
 
+	public ProjectileEffectType effectType;
+	public float modifier;
+
+	public ProjectileEffect(ProjectileEffectType type) {
+		effectType = type;
+	}
+
+	public void applyEffect(Creep creep) {
+	}
+
+	public void setModifier(float modifier) {
+		this.modifier = modifier;
+	}
 }
--- src/towers/ProjectileEffectType.java
+++
@@ -1,7 +0,0 @@
-package towers;
-/*
- * Different kind of effects we can see from projectiles
- */
-public enum ProjectileEffectType {
-	SLOW, SNARE, STUN, DAMAGE_OVER_TIME, MORTAL_STRIKE, ;
-}
