dda8e15b660ebced7850cc24a090c27e6ca81aa4
qsik <qsik777@gmail.com>
2015-05-04 22:09:42 -0500

    Updating projectile class with generic methods

--- src/towers/Projectile.java
+++ src/towers/Projectile.java
@@ -1,10 +1,22 @@
 package towers;
+
+import java.util.HashSet;
+import java.util.Set;
 /*
  * Unit that is fired from a tower, contains information such as position/velocity, target area or target creep
- * as well as tower that fired the projectile. lastly contains the projectile effect which happens when the projectile lands or times out 
+ * as well as tower that fired the projectile. lastly contains the projectile effect which happens when the projectile lands or times out
  * PERHAPS an additional item for stuff like points (bullets) vs rings (aoe circle) vs cones (Flamethrower), or seperate class?
  * Also possibly a second class for passive boost from towers like attack speed in radius
  */
 public class Projectile {
+	public int x, y;
+	public int v;
+
+	public Set<ProjectileEffect> effects;
+
+	public Projectile() {
+		effects = new HashSet<ProjectileEffect>();
+	}
+
 
 }
