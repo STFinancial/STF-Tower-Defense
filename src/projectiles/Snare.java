dd2563721647a1cd000e95d6ead2ee4fdbe49828
Tim Pallarino <palla021@umn.edu>
2015-05-28 23:08:24 -0500

    Re-Added Snare

---
+++ src/projectiles/Snare.java
@@ -0,0 +1,25 @@
+package projectiles;
+
+import creeps.Creep;
+import creeps.Creep.CreepEffect;
+
+public class Snare extends ProjectileEffect {
+
+	public Snare(int lifetime, float modifier) {
+		super(lifetime, modifier);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void applyEffect(Creep creep, CreepEffect effect) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onExpire(Creep creep) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
